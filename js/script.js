dbfc76daf62bd2f2e86678c419d04f53f16ab063
arghmuh5 <armanmuhammad775@gmail.com>
2024-07-03 22:43:33 +0700

    finish

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
 //ini script js//
+
+function replaceName (){
+    let name = prompt("hello, Siapa nama anda?");
+    document.getElementById("name").innerHTML = name
+    
+}
+
+    replaceName();
+
+function validateform(event) {
+    event.preventDefault();
+    const name = document.forms["message-form"]["full-name"].value;
+    const birthDate = document.forms["message-form"]["birth-date"].value;
+    const gender = document.forms["message-form"]["gender"].value;
+    const messages = document.forms["message-form"]["messages"].value;
+
+    if (name == "" || birthDate == "" || gender == "" || messages == "" ){
+        alert("Tidak Boleh Ada Yang Kosong");
+        return false;
+    }
+
+    setSenderUI(name,birthDate,gender,messages);
+
+    return false;
+}
+
+function setSenderUI(name, birthDate, gender, messages) {
+    document.getElementById("sender-full-name").innerHTML = name ;
+    document.getElementById("sender-birth-date").innerHTML = birthDate ;
+    document.getElementById("sender-gender").innerHTML = gender ;
+    document.getElementById("sender-messages").innerHTML = messages ;
+}
+
+
